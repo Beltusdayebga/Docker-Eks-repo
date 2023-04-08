33728a628f4b2e05d3141dbf18e2ab3bf2c5f296
LedouxS <99309845+LedouxS@users.noreply.github.com>
2023-04-08 00:53:56 -0500

    Initial commit.

---
+++ bluewave/terraform/eks-project/teams.tf
@@ -0,0 +1,80 @@
+locals {
+  platform_teams = {
+    admin = {
+      users = concat(
+        [data.aws_caller_identity.current.arn],
+        [
+          "arn:aws:iam::172059043171:user/admin"
+        ]
+      )
+    }
+  }
+
+  application_teams = {
+    platform-services = {
+      "labels" = {
+        "project" = "platform-services"
+      }
+      "quota" = {
+        "requests.cpu"    = "16000m",
+        "requests.memory" = "64Gi",
+        "limits.cpu"      = "16000m",
+        "limits.memory"   = "64Gi",
+        "pods"            = "100",
+        "secrets"         = "500",
+        "services"        = "100"
+      }
+      users = concat(
+        [data.aws_caller_identity.current.arn],
+        [
+          "arn:aws:iam::172059043171:user/admin"
+          
+        ]
+      )
+    }
+  }
+}
+
+# resource "aws_iam_role_policy_attachment" "ecr_policy_attachment_app_teams" {
+#   for_each = module.eks_blueprints.teams[0].application_teams_iam_role_arn
+
+#   role       = split("role/", each.value)[1]
+#   policy_arn = "arn:aws:iam::######:policy/microservices-ecr-access"
+# }
+
+resource "kubernetes_role" "application_teams_admin" {
+  for_each = module.eks_blueprints.teams[0].application_teams_iam_role_arn
+  metadata {
+    name      = "${each.key}-admin-role"
+    namespace = each.key
+  }
+  rule {
+    api_groups = ["*"]
+    resources  = ["configmaps", "pods", "podtemplates", "secrets", "serviceaccounts", "services", "deployments", "horizontalpodautoscalers", "networkpolicies", "statefulsets", "replicasets", "ingresses", "jobs", "cronjobs"]
+    verbs      = ["create", "update", "delete", "get", "list", "patch"]
+  }
+  rule {
+    api_groups = ["*"]
+    resources  = ["resourcequotas"]
+    verbs      = ["create", "update", "delete"]
+  }
+}
+
+resource "kubernetes_role_binding" "application_teams_admin" {
+  for_each = module.eks_blueprints.teams[0].application_teams_iam_role_arn
+  metadata {
+    name      = "${each.key}-admin-role-binding"
+    namespace = each.key
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "${each.key}-admin-role"
+  }
+  subject {
+    kind      = "Group"
+    name      = "${each.key}-group"
+    api_group = "rbac.authorization.k8s.io"
+    namespace = each.key
+  }
+}
